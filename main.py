851049d9bb1e4f74d3e0e1275133c3631db14316
FatimaHabib <56344114+FatimaHabib@users.noreply.github.com>
2025-03-06 11:27:13 +0100

    data validation completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -7,7 +8,19 @@ try:
     logger.info(f">>>>>>> Starting {STAGE_NAME} <<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f"{STAGE_NAME} completed successfully")
+    logger.info(f">>>>>>> {STAGE_NAME} completed successfully <<<<<<< ")
+
+except Exception as e:
+    logger.error(f"An error occurred during {STAGE_NAME}: {str(e)}")
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>> Starting {STAGE_NAME} <<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>> {STAGE_NAME} completed successfully <<<<<<< ")
 
 except Exception as e:
     logger.error(f"An error occurred during {STAGE_NAME}: {str(e)}")
